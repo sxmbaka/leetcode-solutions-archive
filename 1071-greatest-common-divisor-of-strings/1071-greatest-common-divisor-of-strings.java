8297753f0f82f2d7f553e010087cef738bbef098
Yatharth Singh <96312006+yatharthw@users.noreply.github.com>
2023-02-01 23:39:34 +0530

    Time: 1 ms (85.83%), Space: 41.9 MB (82.48%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,19 +1,4 @@
 class Solution {
-    public boolean divides(String a, String b) {
-        int la = a.length(), lb = b.length();
-        if (la % lb != 0) {
-            return false;
-        }
-        int start = 0, end = lb;
-        while (end <= la) {
-           if (!b.equals(a.substring(start, end))) {
-               return false;
-           }
-           start += lb;
-           end += lb;
-        }
-        return true;
-    }
     public int gcd(int x, int y) {
         int gcd = 1;
         for(int i = 1; i <= x && i <= y; i++) {
@@ -23,15 +8,6 @@ class Solution {
         return gcd;
     }
     public String gcdOfStrings(String str1, String str2) {
-        System.out.println(divides("CODE", "LEET"));
-        int i = gcd(str1.length(), str2.length());
-        while (i >= 1) {
-            String div = new String(str1.substring(0, i));
-            if (divides(str1, div) && divides(str2, div)) {
-                return div;
-            }
-            i--;
-        }
-        return "";
+        return ((str1 + str2).equals(str2 + str1))? str1.substring(0, gcd(str1.length(), str2.length())) : "";
     }
 }
