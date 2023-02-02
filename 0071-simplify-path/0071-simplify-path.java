149c67dc3de8b64b937792a8bdc8b279c7de05a2
Yatharth Singh <96312006+yatharthw@users.noreply.github.com>
2023-02-02 16:34:51 +0530

    Time: 9 ms (33.55%), Space: 42.6 MB (55.52%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,50 +1,26 @@
 class Solution {
     public String simplifyPath(String path) {
-        if (path.equals("/")) {
-            return path;
-        }
-        int i = 0, len = path.length(), start, end;
-        String dir;
-        Stack<String> respath = new Stack<>();
-        while (i < len) {
-            if (path.charAt(i) == '/') {
-                while (i < len && path.charAt(i) == '/') {
-                    i++;
+        Stack<String> s = new Stack<>();
+        String[] p = path.split("/");
+        for (int i = 0; i < p.length; i++) {
+            if (p[i].equals("..")) {
+                if (!s.empty()) {
+                    s.pop();
                 }
-            }
-            start = i;
-            if (start == len) {
-                break;
-            }
-            while (i < len && path.charAt(i) != '/') {
-                i++;
-            }
-            end = i - 1;
-            System.out.println("start = " + start + " end = " + end);
-            dir = path.substring(start, end+1);
-            System.out.println(dir);
-            if (dir.equals(".")) {
+            } else if (p[i].equals(".")) {
                 ;
-            } else if (dir.equals("..")) {
-                if (!respath.empty()) {
-                    respath.pop();
-                }
-            } else {
-                respath.push(dir);
+            } else if (!p[i].equals("")) {
+                s.push(p[i]);
             }
-            i++;
         }
-        if (respath.empty()) {
+        if (s.empty()) {
             return "/";
         }
-        if (respath.peek().equals("") || respath.peek().equals(" ")) {
-            respath.pop();
-        }
-        String result = "";
-        Iterator val = respath.iterator();
+        String res = "";
+        Iterator val = s.iterator();
         while (val.hasNext()) {
-            result += "/" + val.next();
+            res += "/" + val.next();
         }
-        return result;
+        return res;
     }
 }
