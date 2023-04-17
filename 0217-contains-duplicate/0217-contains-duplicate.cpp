d971bebc49aa472b76cc430e316815d71e4af81b
Yatharth Singh <96312006+sxmbaka@users.noreply.github.com>
2023-04-17 11:17:10 +0530

    Time: 144 ms (52.06%), Space: 69.4 MB (60.80%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,9 +4,7 @@ public:
         unordered_map<int, int> m;
         for (auto i : nums) {
             m[i]++;
-        }
-        for (auto i : m) {
-            if (i.second > 1)
+            if (m[i] > 1)
                 return true;
         }
         return false;
