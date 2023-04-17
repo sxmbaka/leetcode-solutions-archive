32fe2a195c7c800ccf8018907f880070f018c330
Yatharth Singh <96312006+sxmbaka@users.noreply.github.com>
2023-04-17 12:25:57 +0530

    Time: 36 ms (72.23%), Space: 19.8 MB (74.59%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    string sorted(string s) {
-        sort(s.begin(), s.end());
-        return s;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
-        string key;
-        unordered_map<string, vector<string>> m;
+        long long key;
+        unordered_map<long long, vector<string>> m;
+        vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
         for (auto i : strs) {
-            key = sorted(i);
+            key = 1;
+            for (auto c : i)
+                key = (key * primes[c - 97]) % 1000000007;
             m[key].push_back(i);
         }
-        for (auto i : m) {
-            ans.push_back(i.second);
-        }
+        for (auto i : m) ans.push_back(i.second);
         return ans;
     }
 };
