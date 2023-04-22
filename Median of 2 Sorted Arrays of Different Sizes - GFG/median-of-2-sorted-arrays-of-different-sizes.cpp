29ed9b5e0eccfbb74def945ce53afae1909809da
Yatharth Singh <96312006+sxmbaka@users.noreply.github.com>
2023-04-22 13:27:52 +0530

    Added solution - LeetHub

---
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+double MedianOfArrays(vector<int>& array1, vector<int>& array2);
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    double MedianOfArrays(vector<int>& nums1, vector<int>& nums2) {
+        int N1 = nums1.size();
+        int N2 = nums2.size();
+        if (N1 < N2) return MedianOfArrays(nums2, nums1);	// Make sure A2 is the shorter one.
+        
+        int lo = 0, hi = N2 * 2;
+        while (lo <= hi) {
+            int mid2 = (lo + hi) / 2;   // Try Cut 2 
+            int mid1 = N1 + N2 - mid2;  // Calculate Cut 1 accordingly
+            
+            double L1 = (mid1 == 0) ? INT_MIN : nums1[(mid1-1)/2];	// Get L1, R1, L2, R2 respectively
+            double L2 = (mid2 == 0) ? INT_MIN : nums2[(mid2-1)/2];
+            double R1 = (mid1 == N1 * 2) ? INT_MAX : nums1[(mid1)/2];
+            double R2 = (mid2 == N2 * 2) ? INT_MAX : nums2[(mid2)/2];
+            
+            if (L1 > R2) lo = mid2 + 1;		// A1's lower half is too big; need to move C1 left (C2 right)
+            else if (L2 > R1) hi = mid2 - 1;	// A2's lower half too big; need to move C2 left.
+            else return (max(L1,L2) + min(R1, R2)) / 2;	// Otherwise, that's the right cut.
+        }
+        return -1;
+    }    
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        cin>>n;
+        vector<int> array2(n);
+        for (int i = 0; i < n; ++i){
+            cin>>array2[i];
+        }
+        Solution ob;
+        cout<<ob.MedianOfArrays(array1, array2)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
