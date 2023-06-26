b4879c1980d3724498e718420f0cce7e50a2c418
Yatharth Singh <96312006+sxmbaka@users.noreply.github.com>
2023-06-26 13:38:26 +0530

    Time: 204 ms (66.96%), Space: 75.1 MB (46.82%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -4,27 +4,27 @@ public:
         priority_queue<int, vector<int>, greater<int>> left, right;
         deque<int> dq;
         long long i = 0, j = costs.size() - 1, result = 0, n = costs.size();
-        cout << n << endl;
+        // cout << n << endl;
         while (i < candidates and i < n) left.push(costs[i++]);
         while (j >= costs.size() - candidates and j >= 0 and j > i) right.push(costs[j--]);
         while (i <= j) dq.push_back(costs[i++]);
         while (k--) {
             if (right.empty() or left.top() <= right.top() and !left.empty()) {
-                cout << "adding " << left.top() << " from left" << endl;
+                // cout << "adding " << left.top() << " from left" << endl;
                 result += left.top();
                 left.pop();
                 if (dq.size()) {
-                    cout << "removing " << dq.front() << " from front" << endl;
+                    // cout << "removing " << dq.front() << " from front" << endl;
                     int leftMostElement = dq.front();
                     left.push(leftMostElement);
                     dq.pop_front();
                 }
             } else if (!right.empty()) {
-                cout << "adding " << left.top() << " from right" << endl;
+                // cout << "adding " << left.top() << " from right" << endl;
                 result += right.top();
                 right.pop();
                 if (dq.size()) {
-                    cout << "removing " << dq.back() << " from back" << endl;
+                    // cout << "removing " << dq.back() << " from back" << endl;
                     int rightMostElement = dq.back();
                     right.push(rightMostElement);
                     dq.pop_back();
