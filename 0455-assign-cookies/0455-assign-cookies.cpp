f226d1fdc822fc31f847f5d2055cc5b2778e95d6
Yatharth Singh <96312006+sxmbaka@users.noreply.github.com>
2024-01-01 18:43:13 +0530

    Time: 51 ms (7.77%), Space: 17.9 MB (36.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,25 +1,42 @@
+// class Solution {
+// public:
+//     int findContentChildren(vector<int>& g, vector<int>& s) {
+//         if (s.size() == 0) return 0;
+//         sort(g.begin(), g.end());
+//         sort(s.begin(), s.end());
+//         int largestCandyLeft = s.size() - 1;
+//         int largestGreedLeft = g.size() - 1;
+//         int happyChildren = 0;
+//         while (true) {
+//             if (s[largestCandyLeft] >= g[largestGreedLeft]) {
+//                 happyChildren++;
+//                 largestCandyLeft--;
+//                 largestGreedLeft--;
+//             }
+//             else if (s[largestCandyLeft] < g[largestGreedLeft]) {
+//                 largestGreedLeft--;
+//             }
+//             if (largestCandyLeft == -1 or largestGreedLeft == -1) {
+//                 break;
+//             }
+//         }
+//         return happyChildren;
+//     }
+// };
+
 class Solution {
 public:
-    int findContentChildren(vector<int>& g, vector<int>& s) {
-        if (s.size() == 0) return 0;
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-        int largestCandyLeft = s.size() - 1;
-        int largestGreedLeft = g.size() - 1;
-        int happyChildren = 0;
-        while (true) {
-            if (s[largestCandyLeft] >= g[largestGreedLeft]) {
-                happyChildren++;
-                largestCandyLeft--;
-                largestGreedLeft--;
+    int findContentChildren(std::vector<int>& g, std::vector<int>& s) {
+        std::sort(g.begin(), g.end());
+        std::sort(s.begin(), s.end());
+        int contentChildren = 0;
+        int cookieIndex = 0;
+        while (cookieIndex < s.size() && contentChildren < g.size()) {
+            if (s[cookieIndex] >= g[contentChildren]) {
+                contentChildren++;
             }
-            else if (s[largestCandyLeft] < g[largestGreedLeft]) {
-                largestGreedLeft--;
-            }
-            if (largestCandyLeft == -1 or largestGreedLeft == -1) {
-                break;
-            }
+            cookieIndex++;
         }
-        return happyChildren;
+        return contentChildren;
     }
 };
