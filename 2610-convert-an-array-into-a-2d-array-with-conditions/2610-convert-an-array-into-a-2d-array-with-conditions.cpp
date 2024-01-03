70b2122977aba2c0ad6ebbccea799844dc66a2e1
Yatharth Singh <96312006+sxmbaka@users.noreply.github.com>
2024-01-03 10:46:41 +0530

    Time: 22 ms (12.86%), Space: 29.7 MB (97.82%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,43 @@
+// 1. Iterate through the array
+// 2. If the result is empty
+class Solution {
+private:
+    bool isInArray(vector<int>& arr, int num) {
+        for (auto i : arr) {
+            if (num == i) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void printArr(vector<vector<int>>& arr) {
+        for (auto row : arr) {
+            for (auto i : row) {
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+    }
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<vector<int>> res;
+        for (int j = 0; j < nums.size(); j++) {
+            bool added = false;
+            for (int i = 0; i < res.size(); i++) {
+                if (!isInArray(res[i], nums[j])) {
+                    // cout << "added " << nums[j] << " to " << i << "th row " << endl;
+                    res[i].push_back(nums[j]);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                // cout << "added " << nums[j] << " to new row" << endl;
+                res.push_back({nums[j]});
+            }
+            // printArr(res);
+        }
+        return res;
+    }
+};
